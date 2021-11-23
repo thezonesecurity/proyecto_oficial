593917347a93b0c8e1e08bded40bfe7026e52f94
Olga <adanasialee@gmail.com>
2021-11-22 20:16:22 -0400

    test de servidor

--- client/src/admin/SubSidebar.js
+++ client/src/admin/SubSidebar.js
@@ -4,68 +4,70 @@ import { Link } from "react-router-dom";
 export const SubSidebar = () => {
   return (
     <div className="subSidebar">
-      <ul className="nav flex-column">
-        <li className="nav-item">
-          <Link
-            to="/docente"
-            className="btn btn-outline-info"
-            aria-current="page"
-          >
-            Docente
-          </Link>
-        </li>
-        <br />
-        <li className="nav-item">
-          <Link
-            to="/semestre"
-            className="btn btn-outline-info"
-            aria-current="page"
-          >
-            Semestre
-          </Link>
-        </li>
-        <br />
-        <li className="nav-item">
-          <Link
-            to="/materia"
-            className="btn btn-outline-info"
-            aria-current="page"
-          >
-            Materias
-          </Link>
-        </li>
-        <br />
-        <li className="nav-item">
-          <Link
-            to="/ambiente"
-            className="btn btn-outline-info"
-            aria-current="page"
-          >
-            Ambientes
-          </Link>
-        </li>
-        <br />
-        <li className="nav-item">
-          <Link
-            to="/horario"
-            className="btn btn-outline-info"
-            aria-current="page"
-          >
-            Horarios
-          </Link>
-        </li>
-        <br />
-        <li className="nav-item">
-          <Link
-            to="/reporte"
-            className="btn btn-outline-info"
-            aria-current="page"
-          >
-            Reportes
-          </Link>
-        </li>
-        <br></br>
-      </ul>
+      <div>
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <Link
+              to="/docente"
+              className="btn btn-outline-info"
+              aria-current="page"
+            >
+              Docente
+            </Link>
+          </li>
+          <br />
+          <li className="nav-item">
+            <Link
+              to="/semestre"
+              className="btn btn-outline-info"
+              aria-current="page"
+            >
+              Semestre
+            </Link>
+          </li>
+          <br />
+          <li className="nav-item">
+            <Link
+              to="/materia"
+              className="btn btn-outline-info"
+              aria-current="page"
+            >
+              Materias
+            </Link>
+          </li>
+          <br />
+          <li className="nav-item">
+            <Link
+              to="/ambiente"
+              className="btn btn-outline-info"
+              aria-current="page"
+            >
+              Ambientes
+            </Link>
+          </li>
+          <br />
+          <li className="nav-item">
+            <Link
+              to="/horario"
+              className="btn btn-outline-info"
+              aria-current="page"
+            >
+              Horarios
+            </Link>
+          </li>
+          <br />
+          <li className="nav-item">
+            <Link
+              to="/reporte"
+              className="btn btn-outline-info"
+              aria-current="page"
+            >
+              Reportes
+            </Link>
+          </li>
+          <br></br>
+        </ul>
+      </div>
     </div>
   );
 };
